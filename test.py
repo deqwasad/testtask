6ce21aa15327496984612ebd5250276bd8199f84
deqwasad <99531869+deqwasad@users.noreply.github.com>
2022-02-24 21:38:43 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,37 @@
 from random import randint
-n = int(input())
-a = []
-c = []
-k = 0
-for i in range(n):
-    m = randint(1, n)
-    while k < len(c):
-        if m == c[k]:
-            k = 0
-            m = randint(1, n)
-        else:
-            k += 1
-    c.append(m)
-    k = 0
-    b = []
-    for j in range(m):
-        x = randint(1, n)
-        b.append(x)
-    z = i % 2
-    if z == 1:
-        b = sorted(b)
+
+
+def sorting(sorting_i, sorting_array_secondary, sorting_array_final):  # сортировка размера, с которым проводится работа
+    if sorting_i % 2 == 1:
+        array_secondary = sorted(sorting_array_secondary)
     else:
-        b = sorted(b, reverse=True)
-    a.append(b)
-print(c) # размеры полученных массивов
-print(a) # ответ
+        array_secondary = sorted(sorting_array_secondary, reverse=True)
+    sorting_array_final.append(array_secondary)
+    return sorting_array_final
+
+
+def filling(number):  # заполнение массива
+    array_final = []  # финальный массив
+    array_size = []  # массив размеров полученных массивов
+    counter = 0
+    for i in range(number):
+        size_of_array = randint(1, number)  # размер массива, с которым проводится работа
+        while counter < len(array_size):
+            if size_of_array == array_size[counter]:
+                counter = 0
+                size_of_array = randint(1, number)
+            else:
+                counter += 1
+        array_size.append(size_of_array)
+        counter = 0
+        array_secondary = []
+        for j in range(size_of_array):
+            element = randint(1, number)
+            array_secondary.append(element)
+        array_final = sorting(i, array_secondary, array_final)
+    return array_final
+
+
+input_number = int(input())
+answer = filling(input_number)
+print(answer)
